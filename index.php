ed332a870d45616051a553018a04238d62c30764
Romario Alvarado <romarioalvarado@hotmail.es>
2021-09-01 12:34:54 -0600

    Cambios al sistema ya realizados, El sistema ya esta actualizado

--- index.php
+++ index.php
@@ -46,6 +46,7 @@ if (!empty($_SESSION['active'])) {
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta name="description" content="">
   <meta name="author" content="">
+  
   <title>Sistema de Ventas</title>
   <!-- Custom fonts for this template-->
   <link rel="stylesheet" href="sistema/vendor/bootstrap/css/bootstrap.min.css">
